7e65cdaa47ea655bdfa54fa5c4cc811db795f0f5
superhumangirl <83243361+superhumangirl@users.noreply.github.com>
2021-08-28 12:57:22 -0600

    Add partnerRouter and promotionRouter

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -17,9 +17,30 @@ campsiteRouter.route('/')
     res.statusCode = 403;
     res.end('PUT operation not supported on /campsites');
 })
-
 .delete((req, res) => {
     res.end('Deleting all campsites');
+});
+
+campsiteRouter.route('/:campsiteId')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end(`Will send the info on: ${req.params.campsiteId} to you`);
+})
+.post((req, res) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported for /campsites/${req.params.campsiteId}`);
+})
+.put((req, res) => {
+    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
+    res.end(`Will update the campsite: ${req.body.name}
+        with description: ${req.body.description}`);
+})
+.delete((req, res) => {
+    res.end(`Deleting campsite: ${req.params.campsiteId}`);
 });
 
 module.exports = campsiteRouter;
